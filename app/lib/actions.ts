befeb91b36734f7a210316b5f07ca6263ccbc7ec
Javaria Masooma <javariamasooma.bscssef20@iba-suk.edu.pk>
2025-08-18 16:10:02 +0500

    implemented update and delete mutate on invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -34,3 +34,35 @@ export async function createInvoice(formData : FormData){
     redirect('/dashboard/invoices');
 
 }
+
+
+// UpdateInvoice is used to update an invoice, it omits the id and date fields since they are not needed in the form submission
+// Use Zod to update the expected types
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+ 
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+ 
+  const amountInCents = amount * 100;
+ 
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+ 
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+
+
+
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath('/dashboard/invoices');
+}
